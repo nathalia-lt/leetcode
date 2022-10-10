e22e1306513421f069bacf20a452bdeb01093779
nathalia <nathaliatroina@gmail.com>
2022-10-10 13:22:21 -0400

    max subarray sum

--- sliding window/maxSubarraySum.js
+++ sliding window/maxSubarraySum.js
@@ -22,6 +22,20 @@ maxSubarraySum([2,3], 3) // null
 // Time Complexity - O(N)
 // Space Complexity - O(1)
 
-function maxSubarraySum(){
-    
+function maxSubarraySum(arr, num){
+    if (arr.length < num) return null
+
+    let total = 0 
+    for (let i=0; i < num; i++){
+        
+        total += arr[i]
+    }
+    let currentTotal = total
+    for (let i=num; i < arr.length; i++){
+        currentTotal += arr[i] - arr[i-num]
+        total = Math.max(total, currentTotal)
+    }
+    return total
 }
+
+console.log(maxSubarraySum([100,200,300,400], 2))
