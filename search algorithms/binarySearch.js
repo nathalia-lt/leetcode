52619d0663d3da89d978a9db9016f9d2c9ffe687
nathalia <nathaliatroina@gmail.com>
2022-10-21 15:01:50 -0400

    binary search

--- search algorithms/binarySearch.js
+++ search algorithms/binarySearch.js
@@ -4,7 +4,6 @@
 //and returns the index at which the value exists. Otherwise, return -1.
 
 //Examples:
-
 binarySearch([1,2,3,4,5],2) // 1
 binarySearch([1,2,3,4,5],3) // 2
 binarySearch([1,2,3,4,5],5) // 4
@@ -22,6 +21,29 @@ binarySearch([
   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
 ], 100) // -1
 
-function binarySearch(){
-    
+function binarySearch(arr, val){
+    //I need to set the pointers
+    //start at the very beggining. and end in the very end of course heheh
+    let start = 0
+    let end = arr.length - 1
+    //Math.floor em caso a array nao seja par. eu vou arredondar. nesse caso escolhi arrdondar para baixo
+    let middle = Math.floor(start + end) / 2
+    //Now we are going to get to the logic
+    //we are going to loop
+    while(arr[middle] !== val && start <= end){
+        // then we are going to switch our end point para um dos lados
+        if (val < arr[middle]){
+            end = middle -1
+        }else{
+            start = middle + 1
+        }
+        //agora o middle index vai mudar, porque estamos 'contando uma nova array' novamente
+        middle = Math.floor(start + end) / 2
+    }
+    if (arr[middle] === elem){
+        return middle 
+    }
+    return -1
 }
+
+console.log(binarySearch([1,2,3,4,5],3))
